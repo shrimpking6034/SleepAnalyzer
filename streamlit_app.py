7d75b05b07ed9c698693ff67b2926b79c828bede
Han Wang <whxjtu@gmail.com>
2023-12-29 17:12:35 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 from langchain.chat_models import ChatOpenAI
+from langchain.agents import create_pandas_dataframe_agent
 from langchain_experimental.agents.agent_toolkits import create_pandas_dataframe_agent
 from langchain.agents.agent_types import AgentType
 
@@ -8,6 +9,8 @@ from langchain.agents.agent_types import AgentType
 st.set_page_config(page_title='🦜🔗 Ask the Data App')
 st.title('🦜🔗 Ask the Data App')
 
+prefix = ("You are an expert on sleep quality analysis. The csv data contains two rows: 'Sleep Stage' and "
+          "'Time [hh:mm:ss]'. The data has epoch length of 30 seconds.")
 # Load CSV file
 def load_csv(input_csv):
   df = pd.read_csv(input_csv)
@@ -17,10 +20,10 @@ def load_csv(input_csv):
 
 # Generate LLM response
 def generate_response(csv_file, input_query):
-  llm = ChatOpenAI(model_name='gpt-4-1106-preview', temperature=0.2, openai_api_key=openai_api_key)
+  llm = ChatOpenAI(model_name='gpt-3.5-turbo-0613', temperature=0.2, openai_api_key=openai_api_key)
   df = load_csv(csv_file)
   # Create Pandas DataFrame Agent
-  agent = create_pandas_dataframe_agent(llm, df, verbose=True, agent_type=AgentType.OPENAI_FUNCTIONS)
+  agent = create_pandas_dataframe_agent(llm, df, verbose=True, agent_type=AgentType.OPENAI_FUNCTIONS, prefix=prefix)
   # Perform Query using the Agent
   response = agent.run(input_query)
   return st.success(response)
